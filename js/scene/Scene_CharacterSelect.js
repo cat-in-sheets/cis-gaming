6189466343ccca8731c416df39354e234db12f33
[cis] <[cat.in.sheets@gmail.com]>
2023-06-29 02:09:44 +0900

    画像周り

--- js/scene/Scene_CharacterSelect.js
+++ js/scene/Scene_CharacterSelect.js
@@ -1,11 +1,23 @@
 class Scene_CharacterSelect extends Phaser.Scene {
-    constructor() {
-        super('playGame');
-    }
+  constructor() {
+    super('playGame');
+  }
 
-    create() {
-        this.background = this.add.image(0, 0, 'background');
-        this.background.serOrigin(0, 0);
-        this.add.text(20, 20, 'Playing game', { font: '25px Arial', fill: 'yellow' });
-    }
+  create() {
+
+    // 背景表示
+    this.background = this.add.image(config.width / 2, config.height / 2, 'background');
+    // 背景リサイズ
+    this.background.setDisplaySize(config.width, config.height);
+
+    // フィールド表示
+    this.battlefield_1P = this.add.image(config.width / 2, config.height / 2, 'battlefield_1P');
+    this.battlefield_1P.setScale(0.5 ,0.5);
+
+
+    this.add.text(20, 20, 'Playing game', {
+      font: '25px Arial',
+      fill: 'yellow'
+    });
+  }
 }
--- js/scene/Scene_Title.js
+++ js/scene/Scene_Title.js
@@ -1,14 +1,23 @@
 class Scene_Title extends Phaser.Scene {
-    constructor() {
-        super('bootGame');
-    }
+  constructor() {
+    super('bootGame');
+  }
 
-    preload() {
-        this.load.image('background', 'img/VSInvader/background.png');
-    }
+  preload() {
 
-    create() {
-        this.add.text(20, 20, 'Loading game...');
-        this.scene.start('playGame');
-    }
+    // 画像読み込み
+    this.load.image('background', 'img/VSInvader/background.png');
+    this.load.image('battlefield_1P', 'img/VSInvader/battlefield_1P.png');
+    this.load.image('bullet', 'img/VSInvader/bullet.png');
+    this.load.image('enemy_00', 'img/VSInvader/enemy_00.png');
+    this.load.image('item', 'img/VSInvader/item.png');
+    this.load.image('player_1P', 'img/VSInvader/player_1P.png');
+    this.load.image('portal', 'img/VSInvader/portal.png');
+    
+  }
+
+  create() {
+    this.add.text(20, 20, 'Loading game...');
+    this.scene.start('playGame');
+  }
 }
